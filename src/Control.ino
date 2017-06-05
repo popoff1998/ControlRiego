a51fafab08d48f395f1d09035a56a74e52bd4015
Juan Antonio Marín <tonin@uco.es>
2017-06-05 20:34:24 +0200

    Revert "V2 se queda colgado"
    This reverts commit 3f268c36fdd58e129052e10fd2183a80e6f5e6d4.

--- src/Control.ino
+++ src/Control.ino
@@ -219,7 +219,6 @@ void procesaBotones()
                 boton = NULL;
                 while(1) {
                   boton = parseInputs();
-                  yield();
                   if(boton->id == bPAUSE && !boton->estado) break;
                 }
                 ultimosRiegos(HIDE);
@@ -332,7 +331,6 @@ void procesaBotones()
           //Configuramos el idx
           if (Estado.estado == CONFIGURANDO) {
             //Salvamos la posicion del encoder
-            Serial << "Boton IDX en configurando";
             savedValue = value;
             configure->configureIdx(bId2bIndex(boton->id));
             value = boton->idx;
@@ -349,23 +347,16 @@ void procesaEstados()
     case CONFIGURANDO:
       if (boton != NULL) {
         if (boton->flags.action) {
-          Serial << "En estado CONFIGURANDO pulsado ACTION" << endl;
           switch(boton->id) {
             case bMULTIRIEGO:
               break;
             case bPAUSE:
-              if(!boton->estado)
-              {
-                Serial << "Release de Pause" << endl;
-                return;
-              }
+              if(!boton->estado) return;
               if(configure->configuringTime()) {
-                Serial << "SAVE EEPROM TIME" << endl;
                 EEPROM.put(offsetof(__eeprom_data, minutes),minutes);
                 EEPROM.put(offsetof(__eeprom_data, seconds),seconds);
               }
               if(configure->configuringIdx()) {
-                Serial << "SAVE EEPROM IDX" << endl;
                 Boton[configure->getActualIdxIndex()].idx = value;
                 EEPROM.put(offsetof(__eeprom_data, botonIdx[0]) + 2*configure->getActualIdxIndex(),value);
                 value = savedValue;
@@ -385,7 +376,6 @@ void procesaEstados()
           }
         }
       }
-      else procesaEncoder();
       break;
     case ERROR:
       display->blink(DEFAULTBLINK);
@@ -460,7 +450,6 @@ void procesaEncoder()
     Encoder->service();
   #endif
   if(Estado.estado == CONFIGURANDO && configure->configuringIdx()) {
-      Serial << "En procesaencoder idx";
       value -= Encoder->getValue();
       if (value > 1000) value = 1000;
       if (value <  1) value = 1;
